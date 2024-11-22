f4fe00237a58292b311ee922ee34015a98b8dc87
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2024-11-22 21:05:31 +0900

    add springboottest, modify workflow

--- src/test/java/com/sodong/jpa_study/JpaStudyApplicationTest.java
+++ src/test/java/com/sodong/jpa_study/JpaStudyApplicationTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class JpaStudyApplicationTest {
 
     @Test
