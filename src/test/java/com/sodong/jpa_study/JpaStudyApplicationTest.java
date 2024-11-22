56c22cf3fab8bd7f96766e809987b1dd5f2028b6
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2024-11-22 21:01:03 +0900

    remove springboottest

--- src/test/java/com/sodong/jpa_study/JpaStudyApplicationTest.java
+++ src/test/java/com/sodong/jpa_study/JpaStudyApplicationTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JpaStudyApplicationTest {
 
     @Test
